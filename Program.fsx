5598ff2afcc7e44600f122b9d016fabe4cbc65e4
Phaze <gorchalyan@gmail.com>
2022-03-05 17:03:33 +0400

    [HFP] This acronym will stand for

---
+++ Program.fsx
@@ -0,0 +1,22 @@
+﻿open System.Runtime.CompilerServices
+
+printfn "%s" "-----"
+let mutable x = 2
+printfn "%d" x
+x <- 3
+printfn "%d" x
+
+let add y z = y + z
+let increment = add 1
+printfn "%d" (increment 4)
+printfn "%s" "-----"
+
+let super_increment (x:byref<'int>) : unit =
+    x <- x + 1
+
+let super_duper_increment_demonstration =
+    let mutable y = 13
+    super_increment &y
+    printfn "%d" y
+
+super_duper_increment_demonstration
