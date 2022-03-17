621779b9c9b08bac5241fb82d0351d728abce168
Phaze <gorchalyan@gmail.com>
2022-03-18 00:23:16 +0400

    Added newly learned F# code-blocks

--- Program.fsx
+++ Program.fsx
@@ -11,7 +11,7 @@ let increment = add 1
 printfn "%d" (increment 4)
 printfn "%s" "-----"
 
-let super_increment (x:byref<'int>) : unit =
+let super_increment (x:int byref) : unit =
     x <- x + 1
 
 let super_duper_increment_demonstration =
@@ -20,3 +20,46 @@ let super_duper_increment_demonstration =
     printfn "%d" y
 
 super_duper_increment_demonstration
+
+type Employee(id: int, name: string) = 
+    let mutable empId = id
+    let mutable empName = name
+
+    member instance.print =
+        printfn "%d%s%s" empId " " empName
+
+let emp = Employee(1,"Gor")
+emp.print
+
+type Car() as cr = 
+    class
+        let mutable myInt: int = 10
+        member this.addNumbers(x: int, y: int): int = 
+            x + y
+        member this.incrementmyInt(): unit =
+            myInt <- cr.addNumbers(myInt,1)
+        member this.get(): int =
+            myInt
+        member this.set(settledInt: int): unit =
+            myInt <- settledInt
+    end
+
+let car = new Car()
+car.set(0)
+car.incrementmyInt()
+printfn "%d" (car.get())
+
+let mutable carArr: Car array = Array.zeroCreate 3
+let a = Car()
+a.set(1)
+let b = Car()
+b.set(2)
+let c = Car()
+c.set(3)
+
+carArr.[0] <- a
+carArr.[1] <- b
+carArr.[2] <- c
+
+for i in 0..2 do
+    printfn "%d" (carArr.[i].get())
